808a2eff7e7a5339074e30a8e76febda32957355
PaolaTigre <pdtigre@sudamericano.edu.ec>
2023-02-02 12:31:12 -0500

    prueba 3

---
+++ src/components/AntCarEps.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import {  Card } from 'antd';
+
+const { Meta } = Card;
+
+type CardType = {
+    name:string,
+    episode:string;
+    description:string
+}
+
+const AntCardE:React.FC<CardType> = ({name,episode, description}:CardType) => (
+    <Card
+        style={{ width: 200 }}
+        actions={[
+            <SettingOutlined key="setting" />,
+            <EditOutlined key="edit" />,
+            <EllipsisOutlined key="ellipsis" />,
+        ]}
+    >
+        <Meta
+            title={name}
+            description={episode}
+        />
+    </Card>
+);
+
+export default AntCardE;
---
+++ src/pages/Episodes.tsx
@@ -0,0 +1,35 @@
+import {fetchRickAndMorty, rickAndMortyEpisodes} from "../services/rickAndMorty";
+import useSWR from 'swr';
+import React from 'react';
+import AntCardE from "../components/AntCarEps";
+import styles from './character.module.css'
+
+interface EpisodesData {
+    results: Array<{
+        id: number;
+        name: string;
+        episode:string;
+        species:string
+    }>;
+}
+
+export const Episodes: React.FC = () => {
+    const { data } = useSWR<EpisodesData>(rickAndMortyEpisodes, fetchRickAndMorty, {
+        suspense: false,
+    });
+
+    return (
+        <>
+            <h1>Episodes</h1>
+
+            <div className={styles.container}>
+                {data?.results.map((episodes) => (
+                    <AntCardE  key={episodes.id} name={episodes.name} episode={episodes.episode} description={episodes.species}></AntCardE>
+
+                ))}
+            </div>
+
+
+        </>
+    );
+};
